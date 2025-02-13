4d4bc547af71fc9b58b3fdcd4e4222a775965a38
Tyler Vincent <tylerlv3@gmail.com>
2025-02-13 11:44:53 -0800

    one more

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from src.app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_page(client):
+    rv = client.get('/')
+    assert rv.status_code == 200
